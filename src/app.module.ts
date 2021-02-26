344cdca8de153ca5b0eab926de8fde55fa2f7255
Rafaela Leal Martins <rafaelalealmartinss@gmail.com>
2021-02-26 11:48:24 -0300

    add products crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './product/products.controller';
+import { ProductsModule } from './product/products.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://rafaela:***@cluster0.2vd3s.mongodb.net/beerstore?retryWrites=true&w=majority')],
-  controllers: [AppController],
+  imports: [MongooseModule.forRoot('mongodb+srv://rafaela:***@cluster0.2vd3s.mongodb.net/beerstore?retryWrites=true&w=majority'), ProductModule],
+  controllers: [AppController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/product/products.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ProductsController } from './products.controller';
+import { ProductSchema } from './product.model';
+import { ProductsService } from './products.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{name: 'Product', schema: ProductSchema}])],
+  controllers: [ProductsController],
+  providers: [ProductsService]
+})
+export class ProductsModule {}
